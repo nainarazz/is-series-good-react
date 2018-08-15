bdeea830dc577997eb2b5bf3d89ce62b666579c5
nainarazz <nainarazz@gmail.com>
2018-08-15 16:36:24 +0300

    added onclick listener when selecting similar shows

--- src/components/SimilarShows/SimilarShowsGrid.js
+++ src/components/SimilarShows/SimilarShowsGrid.js
@@ -6,11 +6,11 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import classes from './similarShows.css';
 import { TMBD_IMAGE_BASE_URL } from '../../api-constants';
+import * as actions from '../../store/actions/tvShow';
 
 class SimilarShowGrid extends Component {
   render() {
     const similarShows = this.props.similarShows;
-    console.log("SIMILAR SHOWS GRID", similarShows);
 
     return (
       <div className={classes.root}>
@@ -19,7 +19,9 @@ class SimilarShowGrid extends Component {
           {similarShows.map(show => (
             <GridListTile className={classes.gridListTile} key={show.poster_path}>
               <img src={show.poster_path ? TMBD_IMAGE_BASE_URL + '/w500' + show.poster_path : ""}
-                alt={show.original_name} />
+                alt={show.original_name} 
+                onClick={() => this.props.showSelected(show)}/>
+
               <GridListTileBar
                 title={show.original_name}
                 classes={{
@@ -27,6 +29,7 @@ class SimilarShowGrid extends Component {
                   title: classes.title,
                 }}
               />
+
             </GridListTile>
           ))}
         </GridList>
@@ -41,4 +44,10 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps, null)(SimilarShowGrid);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    showSelected: (showData) => dispatch(actions.fetchShowDetail(showData.id))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SimilarShowGrid);
