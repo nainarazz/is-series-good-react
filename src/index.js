b77f0bc4806532d0704559adfc3f4b65f57931be
nainarazz <nainarazz@gmail.com>
2018-08-18 22:49:14 +0300

    added redux debugger tool for chrome

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './store/reducers/tvShowReducer';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
